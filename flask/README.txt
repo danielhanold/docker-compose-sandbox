96f885109bc4d2ecdea66ccf65b875400f02b45e
Daniel Hanold <danny@danielhanold.com>
2017-02-24 14:51:22 -0500

    Add terminal command to initialize database

--- flask/README.txt
+++ flask/README.txt
@@ -1,2 +1,4 @@
 This Docker environment uses the Flask development server and is based on this tutorial:
 http://flask.pocoo.org/docs/0.12/tutorial/
+
+The full source cod eis hosted on https://github.com/pallets/flask/tree/master/examples/flaskr.
--- flask/flaskr/flaskr.py
+++ flask/flaskr/flaskr.py
@@ -21,3 +21,30 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+""" Initializes the database """
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialized the database')
+
+""" Helper function to create database connection. The first time the function
+    is called, it will create a database connection for the current context,
+    and successive calls will return the already established connection. """
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+""" Helper function to close database at the end of the request. Flask provides
+    us with the teardown_appcontext() decorator. It's executed every time
+    the application context tears down. """
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
