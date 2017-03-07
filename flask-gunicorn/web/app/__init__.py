92cd0f28fc5da9e7a31b2578dea89fe9dca83727
Daniel Hanold <danny@danielhanold.com>
2017-03-07 16:56:14 -0500

    Add Flask Auth

--- flask-gunicorn/web/app/__init__.py
+++ flask-gunicorn/web/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_admin import Admin
+from flaskext.auth import Auth
 
 # Instantiate a new variable by creating a Flask object.
 app = Flask(__name__)
 
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
+auth = Auth(app)
 
 # Import configuration.
 app.config.from_object('config')
