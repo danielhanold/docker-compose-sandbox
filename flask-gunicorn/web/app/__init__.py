ad5ee6716bca5d7d286cb9ea56308ce92968c242
Daniel Hanold <danny@danielhanold.com>
2017-03-07 16:53:17 -0500

    Add Flask Admin

--- flask-gunicorn/web/app/__init__.py
+++ flask-gunicorn/web/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_admin import Admin
 
 # Instantiate a new variable by creating a Flask object.
 app = Flask(__name__)
+
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 # Import configuration.
 app.config.from_object('config')
