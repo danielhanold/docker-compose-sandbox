cb1d8f3083985663319869d53cd0e7b80e7c2522
Daniel Hanold <danny@danielhanold.com>
2017-03-09 15:25:19 -0500

    Update Elasticsearch import script

--- django-nomissez/config/elastic_search/import_story.sh
+++ django-nomissez/config/elastic_search/import_story.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-machineAddress=$(docker-machine ip dev)
 unzip -q ./backups.zip -d ./
-curl -XPUT http://$machineAddress:9200/_snapshot/test-elastic-backup -d '{"type":"fs","settings":{"compress": true,"location":"test-elastic"}}'
-curl -XPOST http://$machineAddress:9200/_snapshot/test-elastic-backup/mars72017/_restore
+curl -XPUT http://localhost:9200/_snapshot/test-elastic-backup -d '{"type":"fs","settings":{"compress": true,"location":"test-elastic"}}'
+curl -XPOST http://localhost:9200/_snapshot/test-elastic-backup/mars72017/_restore
