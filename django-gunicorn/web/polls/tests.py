e269f75f78b74542481f048e1d8334348abc4523
Daniel Hanold <danny@danielhanold.com>
2017-04-11 11:57:42 -0400

    Add more tests for detail of polls app

--- django-gunicorn/web/polls/tests.py
+++ django-gunicorn/web/polls/tests.py
@@ -101,3 +101,26 @@ class QuestionMethodTests(TestCase):
         recent_question = Question(pub_date=time)
 
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+         should return a 4040 not found.
+        """
+        future_question = create_question(question_text='Future Question.', days=5)
+        url = reverse('polls:view_detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        should display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:view_detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
