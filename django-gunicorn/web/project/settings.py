6736d828fb1416e8abe6bae26895fddf3b467637
Daniel Hanold <danny@danielhanold.com>
2017-04-12 16:45:52 -0400

    Create new app for Django topics tutorials

--- django-gunicorn/web/project/settings.py
+++ django-gunicorn/web/project/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     #'debug_toolbar',
+    'topics.apps.TopicsConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
