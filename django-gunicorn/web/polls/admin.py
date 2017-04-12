4891bbbacfe10e2b7dfe4bb7fe4f8a2db7813619
Daniel Hanold <danny@danielhanold.com>
2017-04-12 14:09:49 -0400

    Add Choices to Django admin

--- django-gunicorn/web/polls/admin.py
+++ django-gunicorn/web/polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -20,3 +20,4 @@ class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
