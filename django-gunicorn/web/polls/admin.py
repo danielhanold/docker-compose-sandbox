dc52a8ecb01a1422470d7f529600992dcacaf0c4
Daniel Hanold <danny@danielhanold.com>
2017-04-12 14:12:07 -0400

    Add Choices to Questions page

--- django-gunicorn/web/polls/admin.py
+++ django-gunicorn/web/polls/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+
 class QuestionAdmin(admin.ModelAdmin):
 
     # Example: Create a fieldset and put specific fields
@@ -15,9 +22,9 @@ class QuestionAdmin(admin.ModelAdmin):
             'fields': ['pub_date']
         })
     ]
+    inlines = [ChoiceInline]
 
     # Example: Set the order of fields to appear, no fieldsets.
     # fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
