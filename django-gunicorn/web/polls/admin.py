500c0084b64f2d5de992500eacd950084ef7365f
Daniel Hanold <danny@danielhanold.com>
2017-04-12 14:05:44 -0400

    Add fieldset to Django admin

--- django-gunicorn/web/polls/admin.py
+++ django-gunicorn/web/polls/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    # Example: Create a fieldset and put specific fields
+    # into a fieldset.
+    fieldsets = [
+        (None, {
+            'fields': ['question_text']
+        }),
+        ('Date information', {
+            'fields': ['pub_date']
+        })
+    ]
+
+    # Example: Set the order of fields to appear, no fieldsets.
+    # fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
