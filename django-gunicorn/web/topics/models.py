be648b7a9f3477fbe7753efc402977170efa9731
Daniel Hanold <danny@danielhanold.com>
2017-04-13 13:53:03 -0400

    Add choices option to Person model

--- django-gunicorn/web/topics/models.py
+++ django-gunicorn/web/topics/models.py
@@ -3,6 +3,16 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    shirt_size = models.CharField(
+        max_length=1,
+        choices=SHIRT_SIZES,
+        default='M'
+    )
 
 
 class Musician(models.Model):
@@ -15,7 +25,11 @@ class Musician(models.Model):
 
 
 class Album(models.Model):
+    def __str__(self):
+        return self.name
+
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+
