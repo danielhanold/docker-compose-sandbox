0e333f3be4956686ba20f2df065a1eb355347d2b
Daniel Hanold <danny@danielhanold.com>
2017-04-14 14:47:52 -0400

    Set default values for blog entry

--- django-gunicorn/web/blog/models.py
+++ django-gunicorn/web/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 
 class Blog(models.Model):
@@ -21,12 +22,12 @@ class Entry(models.Model):
     blog = models.ForeignKey(Blog)
     headline = models.CharField(max_length=255)
     body_text = models.TextField()
-    pub_date = models.DateField()
-    mod_date = models.DateField()
+    pub_date = models.DateField(default=date.today)
+    mod_date = models.DateField(default=date.today)
     authors = models.ManyToManyField(Author)
-    n_comments = models.IntegerField()
-    n_pingbacks = models.IntegerField()
-    rating = models.IntegerField()
+    n_comments = models.IntegerField(default=0)
+    n_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
         return self.headline
